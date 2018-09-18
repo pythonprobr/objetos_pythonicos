8672ab501e319f6b5b751512fcdd429ac256de8e
renzon <renzon@gmail.com>
2018-09-18 16:44:49 -0300

    Mapemento

--- programacao_funcional/funcao_como_objeto/filtro_mapa.py
+++ programacao_funcional/funcao_como_objeto/filtro_mapa.py
@@ -1,6 +1,6 @@
 alunos = [('Renzo', 0), ('Luciano', 10), ('Ada', 9)]
 
-print([(nome, nota) for nome, nota in alunos if nota > 5])
+print([nome for nome, nota in alunos if nota > 5])
 
 
 def possui_nota_maior_que_5(aluno):
@@ -9,3 +9,11 @@ def possui_nota_maior_que_5(aluno):
 
 
 print(list(filter(possui_nota_maior_que_5, alunos)))
+
+
+def extrair_nome(aluno):
+    nome, _ = aluno
+    return nome
+
+
+print(list(map(extrair_nome, filter(possui_nota_maior_que_5, alunos))))
