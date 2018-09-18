84d62121b1815959121751912e461d372e3cc892
renzon <renzon@gmail.com>
2018-09-18 17:06:31 -0300

    Exercício: Ordenação Multinível

---
+++ programacao_funcional/funcao_como_objeto/exercicios/ordenar_por_nota_e_nome.py
@@ -0,0 +1,10 @@
+'''
+Construa uma função de ordenacao que ordene os alunos por nota. Se houver empate em nota, o nome
+deverá definir a ordem
+
+>>> alunos = [('Renzo', 0), ('Luciano', 10), ('Renzo Santos', 10), ('Renzo Nuccitelli', 10)]
+>>> alunos.sort(key=ordernar_por_nota_e_nome)
+>>> alunos
+[('Renzo', 0), ('Luciano', 10), ('Renzo Nuccitelli', 10), ('Renzo Santos', 10)]
+
+'''
